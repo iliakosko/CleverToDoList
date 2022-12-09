d5572bcce584724b203d0126d81a7067a4303c4f
ilia kasko <iliakos94@gmail.com>
2022-12-09 16:30:05 +0300

    e

--- to_do/src/App.js
+++ to_do/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 
+import Main from './components/Main';
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-     Hello World
+        <Header>
+          <h1>ASd</h1>
+        </Header>
+        <Main>
+          <h2>dfs</h2>
+        </Main>
     </div>
   );
 }
